737a2773b4b87c931e3c10797eb88b2827aed195
Marlon <marlo@MARLON>
2025-02-03 08:33:22 -0600

    Creacion de la calculadora. Se agrego las opciones de suma, resta, multiplicacion y division.

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,17 +3,15 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
+     RadioGroup rdg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +20,81 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
+                try {
 
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
+                    tempVal = findViewById(R.id.txtNum1);
+                    double num1 = 0;
 
-                double respuesta = num1 + num2;
+                    if (tempVal.getText().toString().isEmpty()){
+                        num1 = 0;
+                    }else {
+                        num1 = Double.parseDouble(tempVal.getText().toString());
+                    }
 
-                tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
+                    tempVal = findViewById(R.id.txtNum2);
+                    double num2 = 0;
+                    double respuesta = 0;
+
+                    if (tempVal.getText().toString().isEmpty()){
+                        num2 = 0;
+                    }else {
+                        num2 = Double.parseDouble(tempVal.getText().toString());
+                    }
+
+
+
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    rdg = findViewById(R.id.radioBtn);
+
+                    if (rdg.getCheckedRadioButtonId() == R.id.radio_suma) {
+                        respuesta = num1 + num2;
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_multiplicacion) {
+                        respuesta = num1 * num2;
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_resta) {
+                        respuesta = num1 - num2;
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_divicion) {
+                        respuesta = num1 / num2;
+                    }
+                    //aqui comienza la segunda parte
+                    else if (rdg.getCheckedRadioButtonId() == R.id.radio_potencia) {
+                        respuesta = Math.pow(num1, num2);
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_porcentaje) {
+                        respuesta = (num1 / num2) * 100;
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_raiz) {
+                        respuesta = calcularRaiz(num1,num2);
+                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_factorial) {
+                        respuesta = factorial((int)num1);
+                    }
+
+                    tempVal.setText("Respuesta: " + respuesta);
+
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
+
         });
+
     }
+
+    public static int factorial(int num) {
+        int resultado = 1;
+        for (int i = 2; i <= num; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+    public static double calcularRaiz(double numero, double indice) {
+        if (numero < 0 && indice % 2 == 0) {
+           return 0.0;
+        }
+        return Math.pow(numero, 1.0 / indice);
+    }
+
 }
