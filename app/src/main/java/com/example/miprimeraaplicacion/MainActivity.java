aec918e64bf9f3293f93c6b0fcc2f1ba6bffc138
Marlon <marlo@MARLON>
2025-02-03 10:30:08 -0600

    Uso del spinner en la calculadora agregando potencia,porcentaje,raiz y factorial

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     Button btn;
     TextView tempVal;
-     RadioGroup rdg;
-
+    Spinner spn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,64 +20,47 @@ public class MainActivity extends AppCompatActivity {
 
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View v) {
-                try {
+                tempVal = findViewById(R.id.txtNum1);
+                double num1 = Double.parseDouble(tempVal.getText().toString());
 
-                    tempVal = findViewById(R.id.txtNum1);
-                    double num1 = 0;
+                tempVal = findViewById(R.id.txtNum2);
+                double num2 = Double.parseDouble(tempVal.getText().toString());
 
-                    if (tempVal.getText().toString().isEmpty()){
-                        num1 = 0;
-                    }else {
-                        num1 = Double.parseDouble(tempVal.getText().toString());
-                    }
+                double respuesta = 0.0;
 
-                    tempVal = findViewById(R.id.txtNum2);
-                    double num2 = 0;
-                    double respuesta = 0;
-
-                    if (tempVal.getText().toString().isEmpty()){
-                        num2 = 0;
-                    }else {
-                        num2 = Double.parseDouble(tempVal.getText().toString());
-                    }
-
-
-
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    rdg = findViewById(R.id.radioBtn);
-
-                    if (rdg.getCheckedRadioButtonId() == R.id.radio_suma) {
+                spn = findViewById(R.id.spnOpciones);
+                switch (spn.getSelectedItemPosition()){
+                    case 0:
                         respuesta = num1 + num2;
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_multiplicacion) {
-                        respuesta = num1 * num2;
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_resta) {
+                        break;
+                    case 1:
                         respuesta = num1 - num2;
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_divicion) {
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
                         respuesta = num1 / num2;
-                    }
-                    //aqui comienza la segunda parte
-                    else if (rdg.getCheckedRadioButtonId() == R.id.radio_potencia) {
+                        break;
+                    case 4:
                         respuesta = Math.pow(num1, num2);
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_porcentaje) {
+                        break;
+                    case 5:
                         respuesta = (num1 / num2) * 100;
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_raiz) {
+                        break;
+                    case 6:
                         respuesta = calcularRaiz(num1,num2);
-                    } else if (rdg.getCheckedRadioButtonId() == R.id.radio_factorial) {
+                        break;
+                    case 7:
                         respuesta = factorial((int)num1);
-                    }
 
-                    tempVal.setText("Respuesta: " + respuesta);
-
-
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
+                        break;
                 }
+                tempVal = findViewById(R.id.lblRespuesta);
+                tempVal.setText("Respuesta: "+ respuesta);
             }
-
         });
 
     }
