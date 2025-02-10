21c743a012865e3b7f9d32df1a4f63d4413eab81
Marlon <marlo@MARLON>
2025-02-10 16:49:49 -0600

    Combersores agregados --sin redondeo--

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
     public class MainActivity extends AppCompatActivity {
         TabHost tbh;
@@ -41,81 +43,113 @@ import androidx.appcompat.app.AppCompatActivity;
                     int opcion = tbh.getCurrentTab();
                     int de = 0;
                     int a = 0;
+                    String msg = "";
+                    String TextDe = "";
+                    String TextA ="";
+
                     switch (opcion){
                         case 0:
                             spn = findViewById(R.id.spnDeMonedas);
-                             de = spn.getSelectedItemPosition();
+                            de = spn.getSelectedItemPosition();
+                            TextDe = spn.getItemAtPosition(de).toString();
 
                             spn = findViewById(R.id.spnAMonedas);
-                             a = spn.getSelectedItemPosition();
+                            a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 1:
                             spn = findViewById(R.id.spnDeMasa);
                             de = spn.getSelectedItemPosition();
+                            TextDe = spn.getItemAtPosition(de).toString();
 
                             spn = findViewById(R.id.spnAMasa);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 2:
                             spn = findViewById(R.id.spnDeVolumen);
                             de = spn.getSelectedItemPosition();
-
+                            TextDe = spn.getItemAtPosition(de).toString();
                             spn = findViewById(R.id.spnAVolumen);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 3:
                             spn = findViewById(R.id.spnDeLongitud);
                             de = spn.getSelectedItemPosition();
-
+                            TextDe = spn.getItemAtPosition(de).toString();
                             spn = findViewById(R.id.spnALongitud);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 4:
                             spn = findViewById(R.id.spnDeAlmacenamiento);
                             de = spn.getSelectedItemPosition();
-
+                            TextDe = spn.getItemAtPosition(de).toString();
                             spn = findViewById(R.id.spnAAlmacenamiento);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 5:
                             spn = findViewById(R.id.spnDeTiempo);
                             de = spn.getSelectedItemPosition();
-
+                            TextDe = spn.getItemAtPosition(de).toString();
                             spn = findViewById(R.id.spnATiempo);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                         case 6:
                             spn = findViewById(R.id.spnDeTransferenciasDatos);
                             de = spn.getSelectedItemPosition();
-
+                            TextDe = spn.getItemAtPosition(de).toString();
                             spn = findViewById(R.id.spnATransferenciasDatos);
                             a = spn.getSelectedItemPosition();
+                            TextA = spn.getItemAtPosition(a).toString();
                             break;
                     }
 
-
-
                     tempVal = findViewById(R.id.txtCantidad);
-                    double cantidad = Double.parseDouble(tempVal.getText().toString());
+                    if(tempVal.getText().toString().isEmpty()){
+                        double cantidad = 0;
+                        msg = "La cantidad que ingresaste no es valida";
+                    }
+                    else{
+                        double cantidad = Double.parseDouble(tempVal.getText().toString());
+                        tempVal = findViewById(R.id.lblRespuesta);
 
-                    tempVal = findViewById(R.id.lblRespuesta);
-                    double respuesta = objConversores.convertir(opcion, de, a, cantidad);
-                    tempVal.setText("Respuesta: "+ respuesta);
+                        BigDecimal respuesta = objConversores.convertir(opcion, de, a, cantidad);
+
+                       // respuesta = respuesta.setScale(2, RoundingMode.HALF_UP);
+                        tempVal.setText("Respuesta: "+ respuesta);
+                        msg = "El resultado de combertir " + cantidad + TextDe + " a " + TextA + " : ";
+
+                        Toast.makeText(MainActivity.this, msg + respuesta, Toast.LENGTH_LONG).show();
+                    }
+
                 }
             });
         }
     }
 class conversores{
-    double[][] valores = {
-            {1, 0.98, 7.73, 25.45, 36.78, 508.87, 8.74}, // monedas
-            // {1, 0.98, 7.73, 25.45, 36.78, 508.87, 8.74}, // monedas (comentado)
-            {1, 1000, 1000000, 0.001, 35.274, 2.20462, 0.01, 10, 100, 10000}, // masa
-            {1, 0.000000001, 0.000001, 0.001, 1000, 1057, 264.172, 6.29, 35.3148, 61023.7}, // volumen
-            {1, 1000, 0.01, 0.001, 0.0254, 0.3048, 0.9144, 1609.34, 0.000001, 0.000000001}, // longitud
-            {}, // tiempo
-            {}, // almacenamiento
+    BigDecimal[][] valores = {
+            {new BigDecimal("1"), new BigDecimal("0.92"), new BigDecimal("0.78"), new BigDecimal("110.50"), new BigDecimal("1.27"), new BigDecimal("0.93"), new BigDecimal("1.35"), new BigDecimal("6.36"), new BigDecimal("20.50"), new BigDecimal("5.25")}, // monedas
+            {new BigDecimal("1"), new BigDecimal("1000"), new BigDecimal("1000000"), new BigDecimal("0.001"), new BigDecimal("35.274"), new BigDecimal("2.20462"), new BigDecimal("0.01"), new BigDecimal("10"), new BigDecimal("100"), new BigDecimal("10000")}, // masa
+            {new BigDecimal("1"), new BigDecimal("0.000000001"), new BigDecimal("0.000001"), new BigDecimal("0.001"), new BigDecimal("1000"), new BigDecimal("1057"), new BigDecimal("264.172"), new BigDecimal("6.29"), new BigDecimal("35.3148"), new BigDecimal("61023.7")}, // volumen
+
+            {new BigDecimal("1"), new BigDecimal("0.001"), new BigDecimal("100"), new BigDecimal("1000"), new BigDecimal("39.3701"), new BigDecimal("3.28084"), new BigDecimal("1.09361"), new BigDecimal("0.000621371"), new BigDecimal("1000000"), new BigDecimal("1000000000")}, // longitud
+
+            {new BigDecimal("1"), new BigDecimal("8"), new BigDecimal("8192"), new BigDecimal("8388608"), new BigDecimal("8589934592"), new BigDecimal("8796093022208"), new BigDecimal("9007199254740992"), new BigDecimal("9223372036854775808"), new BigDecimal("9444732965739290427392"), new BigDecimal("9671406556917033397649408")}, // Almacenamiento
+
+            {new BigDecimal("1"), new BigDecimal("0.0167"), new BigDecimal("0.000278"), new BigDecimal("0.0000116"),  new BigDecimal("0.00000165"), new BigDecimal("0.0000000317"), new BigDecimal("0.000000000317"), new BigDecimal("0.00000000317"), new BigDecimal("0.00000000634"), new BigDecimal("0.0000000106")}, // tiempo
+
+            {new BigDecimal("1"), new BigDecimal("0.125"), new BigDecimal("0.001"), new BigDecimal("1e-6"), new BigDecimal("1e-9"), new BigDecimal("1e-12"), new BigDecimal("1e-15"), new BigDecimal("1e-18"), new BigDecimal("1e-21"), new BigDecimal("1e-24")}, // transferencia
+
     };
-    public double convertir(int opcion, int de, int a, double cantidad){
-        return valores[opcion][a] / valores[opcion][de] * cantidad;
+
+    public BigDecimal convertir(int opcion, int de, int a, double cantidad){
+        BigDecimal cantidadBigDecimal = new BigDecimal(String.valueOf(cantidad));
+        BigDecimal resultado = valores[opcion][a].divide(valores[opcion][de], 10, RoundingMode.HALF_UP).multiply(cantidadBigDecimal);
+
+        return resultado;
     }
 }
