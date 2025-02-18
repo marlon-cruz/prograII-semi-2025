1895d2838983b6d818451c94d888df55dcca17d3
Marlon <marlo@MARLON>
2025-02-18 09:53:32 -0600

    Creacion de la rama Sensor_Camara

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -9,51 +9,5 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
     public class MainActivity extends AppCompatActivity {
-        TextView tempVal;
-        Button btn;
-        MediaPlayer mediaPlayer;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            tempVal = findViewById(R.id.lblReproductorMusica);
-            reproductorMusca();
-            btn = findViewById(R.id.btnIniciar);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    iniciar();
-                }
-            });
-            btn = findViewById(R.id.btnPausar);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pausar();
-                }
-            });
-            btn = findViewById(R.id.btnParar);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    detener();
-                }
-            });
-        }
-        void reproductorMusca(){
-            mediaPlayer = MediaPlayer.create(this, R.raw.audio);
-        }
-        void iniciar(){
-            mediaPlayer.start();
-            tempVal.setText("Reproduciendo...");
-        }
-        void pausar(){
-            mediaPlayer.pause();
-            tempVal.setText("Pausado...");
-        }
-        void detener(){
-            mediaPlayer.stop();
-            tempVal.setText("Detenido...");
-            reproductorMusca();
-        }
+
     }
