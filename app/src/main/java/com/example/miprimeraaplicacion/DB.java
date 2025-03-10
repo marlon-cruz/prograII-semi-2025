a565e9bca3dda61acd461c3c6122f03e5e889aa6
Marlon <marlo@MARLON>
2025-03-10 08:50:07 -0600

    Continuamos con el CRUD de amigos

--- app/src/main/java/com/example/miprimeraaplicacion/DB.java
+++ app/src/main/java/com/example/miprimeraaplicacion/DB.java
@@ -1,9 +1,9 @@
 package com.example.miprimeraaplicacion;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class DB extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "amigos";
     private static final int DATABASE_VERSION = 1;
@@ -41,11 +41,9 @@ public class DB extends SQLiteOpenHelper {
         } catch (Exception e) {
             return e.getMessage();
         }
-
     }
-    public Cursor listar_amigos() {
+    public Cursor lista_amigos() {
         SQLiteDatabase db = getReadableDatabase();
         return db.rawQuery("SELECT * FROM amigos", null);
     }
-
 }
--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -1,56 +1,58 @@
 package com.example.miprimeraaplicacion;
 
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
     public class MainActivity extends AppCompatActivity {
-
+        FloatingActionButton fab;
         Button btn;
-        TextView temVal;
-
+        TextView tempVal;
         DB db;
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            btn.findViewById(R.id.btnGuardarAmigo);
 
             db = new DB(this);
+            btn = findViewById(R.id.btnGuardarAmigo);
+            btn.setOnClickListener(view->guardarAmigo());
 
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    buardarAmigo();
-                }
-            });
+            fab = findViewById(R.id.fabListaAmigos);
+            fab.setOnClickListener(view->abrirVentana());
         }
-
-        private void buardarAmigo() {
-                temVal = findViewById(R.id.txtNombre);
-                String nombre = temVal.getText().toString();
-                temVal = findViewById(R.id.txtDireccion);
-                String direccion = temVal.getText().toString();
-                temVal = findViewById(R.id.txtTelefono);
-                String telefono = temVal.getText().toString();
-                temVal = findViewById(R.id.txtEmail);
-                String email = temVal.getText().toString();
-                temVal = findViewById(R.id.txtDui);
-                String dui = temVal.getText().toString();
-
-                String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
-                db.administrar_amigos("agregar", datos);
-            Toast.makeText(getApplicationContext(),"Amigo agredado",Toast.LENGTH_LONG).show();
+        private void abrirVentana(){
+            Intent intent = new Intent(this, lista_amigos.class);
+            startActivity(intent);
         }
+        private void guardarAmigo() {
+            tempVal = findViewById(R.id.txtNombre);
+            String nombre = tempVal.getText().toString();
 
+            tempVal = findViewById(R.id.txtDireccion);
+            String direccion = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtTelefono);
+            String telefono = tempVal.getText().toString();
+            tempVal = findViewById(R.id.txtEmail);
+            String email = tempVal.getText().toString();
+
+            tempVal = findViewById(R.id.txtDui);
+            String dui = tempVal.getText().toString();
+
+            String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
+            db.administrar_amigos("agregar", datos);
+            Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
+            abrirVentana();
+        }
     }
